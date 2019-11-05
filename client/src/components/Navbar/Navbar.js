15507129881ed8b94296e98fbe6cdc8fc49f631c
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-05 14:34:26 -0600

    Semantic ui example on importing elements

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Segment, Menu } from 'semantic-ui-react';
+
+class Navbar extends React.Component {
+
+    state = {};
+
+    render() {
+        return (
+            <Segment>
+                <Menu>
+                    <Menu.Item>
+
+                    </Menu.Item>
+                </Menu>
+            </Segment>
+        )
+    }
+}
+
+export default Navbar;
