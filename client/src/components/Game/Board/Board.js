0fafa324a6d32875b1137f16b2fbff620ee5ee72
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-05 15:28:31 -0600

    temporary gameboard for right now

---
+++ client/src/components/Game/Board/Board.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Square from '../Square/Square'
+
+class Board extends React.Component {
+
+    renderSquare(i) {
+        const x = i % 8;
+        const y = Math.floor(i / 8);
+        const black = (x + y) % 2 === 1;
+        return (
+            <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
+                <Square black={black}></Square>
+            </div>
+        )
+    }
+
+    render() {
+        const squares = [];
+        for (let i = 0; i < 64; i++) {
+            squares.push(this.renderSquare(i))
+        };
+
+        return (
+            <div
+                style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    width: '500px',
+                    height: '500px'
+                }}>
+                    {squares}
+                </div>
+        )
+    }
+}
+
+export default Board;
