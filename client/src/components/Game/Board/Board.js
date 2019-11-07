374fc51629003aafe05a8dc419c41cc627565f78
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-06 20:13:42 -0600

    added pieces using initializing function

--- client/src/components/Game/Board/Board.js
+++ client/src/components/Game/Board/Board.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import Square from '../Square/Square'
+import Square from '../Square/Square';
+import Piece from '../Piece/Piece';
+import initializeBoard from '../../../helper/initializeBoard';
 
 class Board extends React.Component {
 
-    renderSquare(i) {
+    renderSquare(i, item) {
         const x = i % 8;
         const y = Math.floor(i / 8);
         const black = (x + y) % 2 === 1;
+        const piece = item || {icon:null}
         return (
             <div key={i} style={{ width: '12.5%', height: '12.5%' }}>
-                <Square black={black}></Square>
+                <Square black={black}><Piece icon={piece.icon}/></Square>
             </div>
         )
     }
 
+
+
     render() {
+        const arr = initializeBoard();
         const squares = [];
-        for (let i = 0; i < 64; i++) {
-            squares.push(this.renderSquare(i))
-        };
+
+        for (let i = 0; i < arr.length; i++) {
+            squares.push(this.renderSquare(i,arr[i]))
+        }
+
 
         return (
             <div
@@ -28,8 +36,8 @@ class Board extends React.Component {
                     width: '500px',
                     height: '500px'
                 }}>
-                    {squares}
-                </div>
+                {squares}
+            </div>
         )
     }
 }
