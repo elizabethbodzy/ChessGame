c02f60e39438401cc8290fe8c2162f30bb178fa7
Jeffrey Davis <davisj0214@yahoo.com>
2019-11-06 19:27:35 -0600

    merged front end with back end

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Board from './components/Game/Board/Board'
+// import Board from './components/Game/Board/Board'
 import Navbar from './components/Navbar/Navbar';
 import GameContainer from './components/GameContainer/GameContainer'
-import SignUpForm from './components/SignUpForm/SignUpForm';
+// import SignUpForm from './components/SignUpForm/SignUpForm';
 // import './App.css';
 
 function App() {
