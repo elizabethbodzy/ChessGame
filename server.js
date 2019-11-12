6ce1db8102c20340151948759db6a8f6f838e057
Elizabeth Bodzy <bodze087@newschool.edu>
2019-11-11 20:34:23 -0600

    fixing router issue for now

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const { addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
 const PORT = process.env.PORT || 3001;
 // const router = require('./router');
+// hello
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
