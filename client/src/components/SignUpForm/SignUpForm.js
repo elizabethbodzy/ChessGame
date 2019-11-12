de264dd6b6ba4366ed49614df3200423f7e34c29
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-11 18:57:25 -0600

    changed import for the name to signUp

--- client/src/components/SignUpForm/SignUpForm.js
+++ client/src/components/SignUpForm/SignUpForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import API from '../../utils/API';
-import './signup.css';
+import './signUp.css';
 
 class SignUpForm extends Component {
     constructor(props) {
