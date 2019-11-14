4a0d90636c61582aefa24a2295a680a994ba9caa
Jeffrey Davis <davisj0214@yahoo.com>
2019-11-13 21:15:09 -0600

    committing before pulling

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,10 +5,14 @@ import axios from 'axios';
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            activeItem: "home" 
+        this.state = {
+            activeItem: "home"
         };
     };
+
+    componentDidMount() {
+        document.body.style.height = 'inherent';
+    }
 
     handleItemClick = (e, { name }) => this.setState({ activeItem: name });
 
@@ -30,32 +34,34 @@ class Navbar extends Component {
         const { activeItem } = this.state;
 
         return (
-            <Menu inverted>
-                <Menu.Item>
-                    <Image src="./images/header-logo.jpg" />
-                </Menu.Item>
+            <div>
+                <Menu inverted>
+                    <Menu.Item>
+                        <Image src="./images/header-logo.jpg" style={{ width: 50, height: 50 }} />
+                    </Menu.Item>
 
-                <Menu.Item
-                    name="home"
-                    active={activeItem === "home"}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name="messages"
-                    active={activeItem === "messages"}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name="profile"
-                    active={activeItem === "profile"}
-                    onClick={this.handleItemClick}
-                />
-                <Menu.Item
-                    name="signout"
-                    active={activeItem === "sign out"}
-                    onClick={this.handleLogOut}
-                />
-            </Menu>
+                    <Menu.Item
+                        name="home"
+                        active={activeItem === "home"}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name="messages"
+                        active={activeItem === "messages"}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name="profile"
+                        active={activeItem === "profile"}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name="signout"
+                        active={activeItem === "sign out"}
+                        onClick={this.handleLogOut}
+                    />
+                </Menu>
+            </div>
         );
     }
 }
