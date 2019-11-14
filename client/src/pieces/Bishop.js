99741c1de6bfa7e70bd77e3be5cdc8559d9e1b77
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-14 16:21:26 -0600

    created temporary conditions

--- client/src/pieces/Bishop.js
+++ client/src/pieces/Bishop.js
@@ -15,12 +15,56 @@ class Bishop extends Piece {
             const allMoves = [];
             const x = start[0];
             const y = start[1];
+            Loop1: for (let i = x + 1; i < 8; i++) {
 
-            for (let i = x + 1; i < 8; i++) {
-                for (let j = y + 1; j < 8; j++) {
-                    allMoves.push([i,j])
+                Loop2: for (let j = y + 1; j < 8; j++) {
+                    let dx = i - x;
+                    let dy = j - y;
+                    const piece = board[j][i]
+                    if (piece === null) {
+                        if (dx / dy === 1) {
+                            allMoves.push([i, j]);
+                        }
+                    } else if (piece.color === this.color) {
+                        break Loop1;
+                    } else if (piece.color !== this.color) {
+                        if (dx / dy === 1) {
+                            allMoves.push([i, j]);
+                            break Loop1;
+                        }
+                    }
                 }
             }
+
+            // for (let i = x - 1; i >= 0; i--) {
+            //     for (let j = y - 1; j >= 0; j--) {
+            //         let dx = i - x;
+            //         let dy = j - y;
+            //         if (dx / dy === 1) {
+            //             allMoves.push([i, j])
+            //         }
+            //     }
+            // }
+
+            // for (let i = x + 1; i < 8; i++) {
+            //     for (let j = y - 1; j >= 0; j--) {
+            //         let dx = i - x;
+            //         let dy = j - y;
+            //         if (dx / dy === -1) {
+            //             allMoves.push([i, j])
+            //         }
+            //     }
+            // }
+
+            // for (let i = x - 1; i >= 0; i--) {
+            //     for (let j = y + 1; j < 8; j++) {
+            //         let dx = i - x;
+            //         let dy = j - y;
+            //         if (dx / dy === -1) {
+            //             allMoves.push([i, j])
+            //         }
+            //     }
+            // }
 
             console.log(allMoves)
 
