417f8a7a4393fedf58539a9e6c3197607d02a5e8
Fred Nguyen <fred.h.nguyen@gmail.com>
2019-11-17 16:12:28 -0600

    added coordinates

--- client/src/pieces/Queen.js
+++ client/src/pieces/Queen.js
@@ -1,7 +1,7 @@
 import Piece from './piece';
 
 class Queen extends Piece {
-    constructor(player) {
+    constructor(player,x,y) {
         super(player, (player === 1 ? 'https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png' : 'https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png'))
         this.intialPositions = {
             1: [[3, 7]],
